4cafe13498959b76872ae2131816e475cec9fe23
NamelessBystander <astonhogg@outlook.com>
2025-07-12 17:33:02 +1000

    init

---
+++ lua/config/options.lua
@@ -0,0 +1,20 @@
+vim.opt.clipboard = "unnamedplus"
+vim.opt.cursorline = true
+vim.opt.hlsearch = false
+vim.opt.mousescroll = "ver:2"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.smoothscroll = true
+vim.opt.spell = true
+vim.opt.spelloptions = "camel"
+vim.opt.termguicolors = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.undofile = true
+vim.opt.swapfile = false
+vim.opt.ttyfast = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.o.scrolloff = 8;
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,17 @@
+return {
+    { 
+	    "nvim-treesitter/nvim-treesitter",
+	    lazy = false,
+	    build = ":TSUpdate",
+
+	    config = function()
+			require "nvim-treesitter.configs".setup {
+				ensure_installed = { "c", "lua", "rust" , "bash" },
+				sync_install = true,
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+			}
+	    end,
+    },
+}
